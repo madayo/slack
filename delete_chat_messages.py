4d6fb7f87ca95c5f85a62afceb907bf19109f968
YosukeShimada <simayou0615@gmail.com>
2018-01-10 15:45:43 +0900

    python3用に書き換え

--- delete_chat_messages.py
+++ delete_chat_messages.py
@@ -1,47 +1,41 @@
-#coding: utf-8
+#coding utf-8
 
 # slackのメッセージを一括削除する
 # private, publicどちらにも対応しており、対話式で削除対象とするチャンネルを指定する。
 # starがついているものは削除対象外
 # 実行前にプログラム内のtoken変数に適切な値を入力すること https://api.slack.com/custom-integrations/legacy-tokens
 
-import urllib
-import urllib2
+import requests
 import json
 import time
 
-def curl(url, params):
-  params = urllib.urlencode(params)
-  req = urllib2.Request(url)
-  req.add_header('Content-Type', 'application/x-www-form-urlencoded')
-  req.add_data(params)
+def get(url, params):
+  req = requests.get(url, params)
+  return req.json()
 
-  res = urllib2.urlopen(req)
+token = "xoxp-150069085396-150031593506-296521270242-67db28316330e71ab947265b54b3f02f"
+params = {'token' : token}
 
-  body = res.read()
-  return json.loads(body)
+print("channel is private or public? 1:private, 2:public")
+target = "groups" if input("input number >> ") == 1 else "channels"
 
-token = ''
-
-print 'channel is private or public? 1:private, 2:public'
-target = 'groups' if input('input number >> ') == 1 else 'channels'
-
-data = curl("https://slack.com/api/%s.list" % target, { 'token' : token })
+data = get("https://slack.com/api/%s.list" % target, params)
 
 for i, d in enumerate(data[target]):
-  print str(i+1) + " --> " +d["name"]
-print "Which channels messages wanna delete? "
-channel = input('input number >> ')
-channel = data[target][channel-1]["id"]
+  print(str(i+1) + " --> " +d["name"])
+channel = int(input('input number >> ')) - 1
+channel = data[target][channel]["id"]
 
-
-data = curl("https://slack.com/api/%s.history" % target, { 'token' : token, 'channel' : channel, 'count' : 1000 })
+params.update({"channel" : channel})
+data = get("https://slack.com/api/%s.history" % target, params)
 for d in data['messages']:
-  # スター付きは削除しない
+ # スター付きは削除しない
   if "is_starred" in d and d["is_starred"]:
     continue
 
-  result = curl("https://slack.com/api/chat.delete", { 'token' : token, 'channel' : channel, 'ts' : d['ts']})
+  delete_params = params
+  delete_params.update({"ts" : d["ts"]})
+  result = get("https://slack.com/api/chat.delete", delete_params)
   print(result)
-  #連続で送りすぎるとエラーになるので1秒待機
+  # 連続で送りすぎるとエラーになるので1秒待機
   time.sleep(1)
